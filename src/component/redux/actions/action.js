df00d4b52804020d86d52ee7a880d5eb01ca8024
MaharaniJ <maharani.jeyaraman@outlook.com>
2024-01-10 10:52:40 +0530

    jan-10

--- src/component/redux/actions/action.js
+++ src/component/redux/actions/action.js
@@ -2,11 +2,14 @@ import axios from "axios";
 
 export const getProducts = () => async (dispatch) => {
   try {
-    const response = await axios.get("https://ecommerce-w73k.onrender.com/getproducts", {
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
+    const response = await axios.get(
+      "https://ecommerce-w73k.onrender.com/getproducts",
+      {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
 
     // Access the response data using response.data
     console.log(response.data);
